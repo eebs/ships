a7da3d1903bcd92cec817f9e095d4aaedee91dca
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-01-05 01:37:48 -0500

    added status validations

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,4 +2,10 @@ class Status < ActiveRecord::Base
   attr_accessible :color, :name
 
   has_many :runs
+
+  validates :name, :length => { :in => 2..60 }
+
+  validates_each :color do |record, attr, value|
+    record.errors.add(attr, "must be a valid hex color in the form '#XXX' or '#XXXXXX'") unless value =~ /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
+  end
 end
