5529b47278812060f321172a094c78a6d5ccb5db
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-01-11 21:08:20 -0500

    return empty hash of no orders

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -10,17 +10,17 @@ class ApiController < ApplicationController
     orders = @api.orders
     @changes = Order.update_orders(orders)
 
-    open_orders = @api.open_orders
-
     @open_orders_hash = {}
-    open_orders.each do |order|
-      entry = {}
-      entry[:name] = @api.type_name(order.typeID)
-      entry[:price] = order.price
-      entry[:seller] = @api.char_name(order.charID)
-      entry[:volRemaining] = order.volRemaining
-      entry[:duration] = Time.zone.parse(order.issued) + order.duration.to_i.days
-      @open_orders_hash[order.typeID] = entry
+    if open_orders = @api.open_orders
+      open_orders.each do |order|
+        entry = {}
+        entry[:name] = @api.type_name(order.typeID)
+        entry[:price] = order.price
+        entry[:seller] = @api.char_name(order.charID)
+        entry[:volRemaining] = order.volRemaining
+        entry[:duration] = Time.zone.parse(order.issued) + order.duration.to_i.days
+        @open_orders_hash[order.typeID] = entry
+      end
     end
 
     @next_update = @api.next_order_update
