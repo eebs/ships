f70028887e595620f7e6966b94cc95cd203f45e6
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-01-20 14:02:48 -0500

    Don't show reports when using IGB

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,10 +1,19 @@
 class ReportController < ApplicationController
   before_filter :authenticate_character!
+  before_filter :check_igb, :only => :price
 
   def month
   end
 
   def price
     @sold = Run.chart_data
+  end
+
+private
+  def check_igb
+    if igb?
+      flash[:error] = "Reports are currently unavailable when using the in-game browser."
+      redirect_to root_url
+    end
   end
 end
