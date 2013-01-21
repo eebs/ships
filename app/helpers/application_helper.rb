2335941ebd1f0a8f23452c1981cc6ce373d96b81
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-01-21 00:16:30 -0500

    Added character image tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def is_active?(page_name)
         "active" if params[:controller] == page_name
     end
+
+    def character_image_tag(id, size = 32, options = {})
+      image_tag "http://image.eveonline.com/Character/#{id}_#{size}.jpg", options
+    end
 end
