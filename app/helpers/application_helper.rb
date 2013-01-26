68fa01e7ca6490566338f7f17abff4b8418f9628
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-01-26 17:12:15 -0500

    added full_title view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
     def character_image_tag(id, size = 32, options = {})
       image_tag "http://image.eveonline.com/Character/#{id}_#{size}.jpg", options
     end
+
+    # Returns the full title on a per-page basis.
+    def full_title(page_title)
+        base_title = "Koprulu Sector"
+        if page_title.empty?
+            base_title
+        else
+            "#{base_title} | #{page_title}"
+        end
+    end
 end
