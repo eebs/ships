1272dcbc52babd6dfc8d7f30864f71b537de5dad
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-01-26 17:18:51 -0500

    Added title tests, have_title matcher

--- spec/features/the_applications_spec.rb
+++ spec/features/the_applications_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "The application's" do
 
+  let(:base_title) { "Koprulu Sector" }
+
   describe "Home page" do
     
     it "should have the content 'Koprulu Sector'" do
@@ -12,6 +14,24 @@ describe "The application's" do
     it "should have the content 'Home'" do
       visit '/'
       page.should have_content('Home')
+    end
+
+    it "should have the right title" do
+      visit '/'
+      page.should have_title("#{base_title}")
+    end
+  end
+
+  describe "Sign In page" do
+    
+    it "should have the right title" do
+      visit '/characters/sign_in'
+      page.should have_title("#{base_title} | Sign In")
+    end
+
+    it "should have the content 'Sign In'" do
+      visit '/characters/sign_in'
+      page.should have_content('Sign In')
     end
   end
 end
