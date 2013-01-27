ee419b829b2e6662375011485e2ba3281b7761a5
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-01-26 21:06:10 -0500

    cleaned up tests, added general nav tests

--- spec/features/the_applications_spec.rb
+++ spec/features/the_applications_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+include ApplicationHelper
 
 describe "The application's" do
 
@@ -6,19 +7,35 @@ describe "The application's" do
 
   let(:base_title) { "Koprulu Sector" }
 
+  shared_examples_for "all pages" do
+    it { should have_title full_title(page_title) }
+    it { should have_content(heading) }
+    it {should have_selector('a', :text => 'Home')}
+    it {should have_selector('a', :text => 'Koprulu Sector')}
+  end
+
   describe "Home page" do
 
-    before { visit root_path } 
+    let(:page_title) {""}
+    let(:heading) {"Home"}
+
+    before { visit root_path }
+
+    it_should_behave_like "all pages"
     
     it { should have_content 'Koprulu Sector' }
     it { should have_content('Home') }
-    it { should have_title("#{base_title}") }
     it { should_not have_title(" | Home") }
   end
 
   describe "Sign In page" do
 
+    let(:page_title) {"Sign In"}
+    let(:heading) {"Sign In"}
+
     before { visit new_character_session_path }
+
+    it_should_behave_like "all pages"
     
     it { should have_title("#{base_title} | Sign In") }
     it { should have_content('Sign In') }
@@ -26,7 +43,12 @@ describe "The application's" do
 
   describe "Sign Up page" do
 
+    let(:page_title) {"Sign Up"}
+    let(:heading) {"Sign Up"}
+
     before { visit new_character_registration_path }
+
+    it_should_behave_like "all pages"
     
     it { should have_title("#{base_title} | Sign Up") }
     it { should have_content('Sign Up') }
