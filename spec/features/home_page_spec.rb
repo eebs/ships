ccad328633b22868b081cc81cc06b9249c79f724
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-01-30 00:16:22 -0500

    added as guest test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,11 +4,28 @@ include RequestHelper
 
 describe "The home page" do
 
+  describe "as guest" do
+    before { visit root_path }
+    subject { page }
+
+    describe "with no runs" do
+      it "should have content 'No ships need attention'" do
+        should have_content 'No ships need attention'
+      end
+    end
+
+    describe "with runs" do
+    end
+  end
+
   describe "with user" do
     let(:user) { login_user create(:character) }
+    before { visit root_path }
+    subject { page }
 
     it "should have current user" do
       user.should_not be_nil
     end
+
   end
 end
