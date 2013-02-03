decedcad9eb53abc5a3a06ec649850cdd6f57f67
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-02-03 14:36:40 -0500

    added month_date time format

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,3 @@
 Time::DATE_FORMATS[:next_updated] = "Next updated at %I:%M %p %Z, %b %d"
 Time::DATE_FORMATS[:long_twelve_hour] = "%B %e, %Y %l:%M %P"
+Time::DATE_FORMATS[:month_date] = lambda { |time| time.strftime("%b #{time.day.ordinalize}") }
