f61d3cc6361ec37aabd29bee932472093c7a7b48
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-02-05 00:28:48 -0500

    Added reservations actions, views

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,33 @@
 class ReservationsController < ApplicationController
+  before_filter :authenticate_character!
+
   def index
+    @reservations = Reservation.where(:character_id => current_character.id).order('created_at DESC')
+    @runs = @reservations.collect { |r| r.run }
   end
 
   def show
+    @reservation = Reservation.find(params[:id])
   end
 
   def create
+    unless run = Run.find(params[:id])
+      flash[:error] = "Invalid ship specified."
+      return redirect_to root_url
+    end
+
+    if run.reservation
+      flash[:error] = "That ship has already been reserved."
+      return redirect_to root_url
+    end
+
+    @reservation = Reservation.new( :character_id => current_character.id, :run_id => run.id )
+
+    if @reservation.save
+      redirect_to :back, notice: 'Reservation was successfully created.'
+    else
+      redirect_to :back
+    end
   end
 
   def destroy
