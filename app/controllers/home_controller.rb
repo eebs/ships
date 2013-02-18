5975e3ef1e923bfb8fc2d96fa56f3989d8d669ff
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-02-17 21:38:27 -0500

    order by ship_number as well

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @ships = Run.not_sold.joins(:ship).order('ships.name')
+    @ships = Run.not_sold.joins(:ship).order('ships.name').order('ship_number')
   end
 end
