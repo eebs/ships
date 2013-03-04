4b7a12b026ffe39df4fff22aa8a1062fc8b8c381
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-03-03 21:19:50 -0500

    added jobs to admin menu

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,7 @@
 class JobsController < ApplicationController
+  before_filter :authenticate_character!
+  load_and_authorize_resource
+  
   # GET /jobs
   # GET /jobs.json
   def index
@@ -27,6 +30,8 @@ class JobsController < ApplicationController
   # GET /jobs/new
   # GET /jobs/new.json
   def new
+    flash[:alert] = "Manually creating jobs has been disabled"
+    return redirect_to :root
     @job = Job.new
 
     respond_to do |format|
@@ -37,12 +42,16 @@ class JobsController < ApplicationController
 
   # GET /jobs/1/edit
   def edit
+    flash[:alert] = "Manually editing jobs has been disabled"
+    return redirect_to :root
     @job = Job.find(params[:id])
   end
 
   # POST /jobs
   # POST /jobs.json
   def create
+    flash[:alert] = "Manually creating jobs has been disabled"
+    return redirect_to :root
     @job = Job.new(params[:job])
 
     respond_to do |format|
@@ -59,6 +68,8 @@ class JobsController < ApplicationController
   # PUT /jobs/1
   # PUT /jobs/1.json
   def update
+    flash[:alert] = "Manually updating jobs has been disabled"
+    return redirect_to :root
     @job = Job.find(params[:id])
 
     respond_to do |format|
@@ -75,6 +86,8 @@ class JobsController < ApplicationController
   # DELETE /jobs/1
   # DELETE /jobs/1.json
   def destroy
+    flash[:alert] = "Manually deleting jobs has been disabled"
+    return redirect_to :root
     @job = Job.find(params[:id])
     @job.destroy
 
