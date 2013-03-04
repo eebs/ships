c25f38b2e8a10465d8f8d05cd28f80fb2f3ef5c1
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-03-03 20:20:16 -0500

    added finishes and progress bar

--- app/presenters/job_presenter.rb
+++ app/presenters/job_presenter.rb
@@ -15,4 +15,36 @@ class JobPresenter < BasePresenter
     api = EveApi.new
     api.char_name(job.installerID)
   end
+
+  def finishes
+    finishes = Time.zone.parse(job.endProductionTime)
+    finishes.future? ? "#{distance_of_time_in_words_to_now(finishes)} from now" : "#{time_ago_in_words(finishes)} ago"
+  end
+
+  def progress_bar
+    content_tag :div, :class => 'progress progress-info progress-striped' do
+      content_tag :div, '', :class => 'bar', :style => progress_bar_style
+    end    
+  end
+
+  def start_time
+    Time.zone.parse(job.beginProductionTime)
+  end
+
+  def end_time
+    Time.zone.parse(job.endProductionTime)
+  end
+
+private
+
+  def percent_complete
+    total = end_time - start_time
+    total = (total == 0) ? 1 : total
+    completed = Time.zone.now - start_time
+    (completed/total)*100
+  end
+
+  def progress_bar_style
+    "width: #{percent_complete}%;"
+  end
 end
