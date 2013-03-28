901fb30e55b5579eb70a68ef1abd169b76c5a03f
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-03-28 16:46:09 -0400

    updated iso8601 time format

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -2,4 +2,4 @@ Time::DATE_FORMATS[:next_updated] = "Next updated at %I:%M %p %Z, %b %d"
 Time::DATE_FORMATS[:long_twelve_hour] = "%B %e, %Y %l:%M %P"
 Time::DATE_FORMATS[:month_date] = lambda { |time| time.strftime("%b #{time.day.ordinalize}") }
 Time::DATE_FORMATS[:year_month_day] = "%Y-%m-%d"
-Time::DATE_FORMATS[:iso8601] = "%Y-%m-%d %I:%M"
+Time::DATE_FORMATS[:iso8601] = "%Y-%m-%d %H:%M"
