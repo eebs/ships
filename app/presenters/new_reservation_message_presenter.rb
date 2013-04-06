79e2eb91fd54c36c57bf5d311fbfbe31832be305
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-04-06 10:20:57 -0400

    link to reservation not ship

--- app/presenters/new_reservation_message_presenter.rb
+++ app/presenters/new_reservation_message_presenter.rb
@@ -3,7 +3,7 @@ class NewReservationMessagePresenter < BasePresenter
 
   def body
     begin
-      link_to(message.character_name, message.character) + ' has reserved ' + link_to(message.run_name, message.run)
+      link_to(message.character_name, message.character) + ' has reserved ' + link_to(message.run_name, message.run.reservation)
     rescue ActiveRecord::RecordNotFound => e
       "Reservation has been deleted"
     end
